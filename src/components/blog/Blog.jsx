1838939a91d88504937149ea6421c875553367bf
Amr Imam <emama2337@gmail.com>
2024-03-05 19:17:31 +0200

    last

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,7 +2,7 @@ import { FaArrowRight } from "react-icons/fa6"
 import styles from "./styles.module.scss"
 import Picture4 from "../../../public/assets/images/ceo.jpg"
 import video01 from "../../../public/assets/images/tkman.mp4"
-import video02 from "../../../public/assets/images/تمكين.mp4"
+import video02 from "../../../public/assets/images/تمكين.mp4ىحة"
 import Carousel from "react-bootstrap/Carousel"
 import { useState } from "react"
 const Blog = () => {
